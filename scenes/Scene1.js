ef9941e3755bc94527652bae124616a4b23587d0
Tim Winter <mythics.winter@gmail.com>
2021-12-18 16:36:00 -0500

    Uploading minimal image tests

--- scenes/Scene1.js
+++ scenes/Scene1.js
@@ -3,7 +3,12 @@ class Scene1 extends Phaser.Scene {
         super("bootGame");
     }
 
+    preload() {
+        this.preload.image("tiles", "images/tiles.png")
+    }
+
     create() {
         this.add.text(20, 20, "Loading game...");
+        this.scene.start("playGame");
     }
 }
--- scenes/Scene2.js
+++ scenes/Scene2.js
@@ -2,4 +2,8 @@ class Scene1 extends Phaser.Scene {
     constructor() {
         super("playGame");
     }
+
+    create() {
+        this.add.text(20, 20, "Playing game", { font: "25px Arial", fill: "yellow"});
+    }
 }
