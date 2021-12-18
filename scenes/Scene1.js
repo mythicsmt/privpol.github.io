2b32d2129658cbc1460ead7ec1df73ec2ef3ec13
Tim Winter <mythics.winter@gmail.com>
2021-12-18 17:07:29 -0500

    Add background image

--- scenes/Scene1.js
+++ scenes/Scene1.js
@@ -8,6 +8,9 @@ class Scene1 extends Phaser.Scene {
     }
 
     create() {
+        this.background = this.add.image(0, 0, "tiles");
+        this.background.setOrigin(0, 0);
+
         this.add.text(20, 20, "Loading game...");
         this.scene.start("playGame");
     }
