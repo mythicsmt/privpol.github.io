7c059f9f5b6ef2d9238a2243131bce811861c5a2
Tim Winter <mythics.winter@gmail.com>
2021-12-20 08:49:04 -0500

    Removing gitignore and nojekyll. Adding sprite for character, partial sprite for tiles.

--- scenes/Scene1.js
+++ scenes/Scene1.js
@@ -4,7 +4,17 @@ class Scene1 extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image("tiles", "images/tiles.png");
+        //this.load.image("tiles", "images/tiles.png");
+
+        this.load.spritesheet("tiles", "images/tiles.png", {
+            frameWidth = 32,
+            frameHeight = 32
+        });
+
+        this.load.spritesheet("character", "images/character.png", {
+            frameWidth: 16,
+            frameHeight: 22
+        });
     }
 
     create() {
--- scenes/Scene2.js
+++ scenes/Scene2.js
@@ -4,12 +4,22 @@ class Scene2 extends Phaser.Scene {
     }
 
     create() {
-        this.background = this.add.image(0, 0, "tiles");
-        this.background.setOrigin(0, 0);
+        //this.background = this.add.image(0, 0, "tiles");
+        //this.background.setOrigin(0, 0);
 
-        this.add.text(20, 20, "Playing game", { 
-            font: "25px Arial",
-            fill: "yellow"
+        this.character = this.add.sprite(config.width / 2, config.height / 2, "character");
+
+        this.anims.create({
+            key: "character_walk",
+            frames: this.anims.generateFrameNumbers("character"),
+            frameRate: 20,
+            repeat: -1,
+            hideOnComplete: false
         });
+
+        // this.add.text(20, 20, "Playing game", { 
+        //     font: "25px Arial",
+        //     fill: "yellow"
+        // });
     }
 }
